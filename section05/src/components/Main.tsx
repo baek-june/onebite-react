cf7056cfd65981f4df852d38d019519bfc291b18
baek-june <bjsung@stam-p.com>
2024-10-20 17:28:23 +0900

    obj 프로퍼티 접근 표기법

--- section05/src/components/Main.tsx
+++ section05/src/components/Main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export default function Main() {
+  const objA = {
+    a: 1,
+    b: 2,
+  };
   return (
     <main>
       <h1>main</h1>
+      <h2>a: {objA.a}</h2>
+      <h2>b: {objA.b}</h2>
     </main>
   );
 }
